c443f3aeb4ce4344dc271cc624d597420a441e0b
Ryan Wilhelm <ryanmwb@gmail.com>
2020-04-25 19:33:05 -0400

    min change to trigger rebuild

--- apis/brew.js
+++ apis/brew.js
@@ -12,7 +12,7 @@ router.post("/api", (req, res, next) => {
   try {
     const { route, query } = req.body;
     const q = query ? `&q=${query}` : "";
-
+    //
     axios
       .get(`${baseUrl}/${route}/?key=${process.env.BREW_API_KEY}${q}`)
       .then(r => {
