501e9671a027c262fd48feedacceebe138865b76
Ryan Wilhelm <ryanmwb@gmail.com>
2020-04-25 19:47:54 -0400

    min change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/apis", apis, (error, req, res, done) => {
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
