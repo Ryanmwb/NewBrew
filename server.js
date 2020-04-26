2868ed73b4382042ee830fb366944aad2917291b
Ryan Wilhelm <ryanmwb@gmail.com>
2020-04-25 20:37:28 -0400

    min change

--- server.js
+++ server.js
@@ -31,13 +31,15 @@ app.use("/apis", apis, (error, req, res, done) => {
 
 // app.use(express.static("public"));
 // if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
 });
 // } else {
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+// });
 // }
 
 const PORT = process.env.PORT || 5000;
