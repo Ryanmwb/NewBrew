653325b3a1784dda98b1d716d748ba52fa0d2c74
Ryan Wilhelm <ryanmwb@gmail.com>
2020-04-25 20:26:01 -0400

    server change

--- server.js
+++ server.js
@@ -17,21 +17,21 @@ app.use("/apis", apis, (error, req, res, done) => {
   return res.sendStatus(500);
 });
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, "client/build")));
 
-  // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-} else {
-  app.use(express.static("public"));
+//   // Handle React routing, return all requests to React app
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// } else {
+app.use(express.static("public"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+});
+// }
 
 const PORT = process.env.PORT || 5000;
 
