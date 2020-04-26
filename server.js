aa6f91a242d85b18876ccea70524a5acf313d9f7
Ryan Wilhelm <ryanmwb@gmail.com>
2020-04-25 20:49:17 -0400

    min change

--- server.js
+++ server.js
@@ -29,20 +29,16 @@ app.use("/apis", apis, (error, req, res, done) => {
 
 // }
 
-// app.use(express.static("public"));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+// });
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
-  });
-} else {
-  app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
-  });
-}
+// Handle React routing, return all requests to React app
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
