4688b88226ca6470c86516ca55250f86ed842f34
Ryan Wilhelm <ryanmwb@gmail.com>
2020-04-25 20:19:54 -0400

    built client

--- server.js
+++ server.js
@@ -17,11 +17,21 @@ app.use("/apis", apis, (error, req, res, done) => {
   return res.sendStatus(500);
 });
 
-app.use(express.static("public"));
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
-});
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+} else {
+  app.use(express.static("public"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
