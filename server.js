9b30dcb5dd86f30b5009f5d050c4cb732ec070b9
Ryan Wilhelm <ryanmwb@gmail.com>
2020-04-25 20:36:46 -0400

    min change

--- server.js
+++ server.js
@@ -26,8 +26,15 @@ app.use("/apis", apis, (error, req, res, done) => {
 //     res.sendFile(path.join(__dirname, "client/build", "index.html"));
 //   });
 // } else {
-app.use(express.static("public"));
 
+// }
+
+// app.use(express.static("public"));
+// if (process.env.NODE_ENV === "production") {
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+});
+// } else {
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
 });
