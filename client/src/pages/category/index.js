4465c072eb7e90649e8e4e81c2230261952f3602
Ryan Wilhelm <ryanmwb@gmail.com>
2020-04-28 21:07:57 -0400

    working on beer style stats

--- client/src/pages/category/index.js
+++ client/src/pages/category/index.js
@@ -7,14 +7,16 @@ import {
   ExpansionPanelDetails,
   ExpansionPanelSummary,
   Grid,
-  Paper,
+  Tooltip,
   Typography
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 import useStyles from "./styles";
 
 import styles from "../../store/styles";
+import srmColors from "../../store/srmColors";
 
 export default function Category() {
   const classes = useStyles();
@@ -45,23 +47,53 @@ export default function Category() {
                   <ExpansionPanelDetails>
                     <Grid container justify="space-around">
                       <Grid item>
-                        <div className={classes.center}>ABV</div>
                         <div className={classes.center}>
-                          {_.get(style, "abvMin")}-{_.get(style, "abvMax")}
+                          ABV
+                          <Tooltip title="Alcohol by Volume">
+                            <HelpOutlineIcon className={classes.helpIcon} />
+                          </Tooltip>
+                        </div>
+                        <div className={classes.center}>
+                          {_.get(style, "abvMin", "")}-
+                          {_.get(style, "abvMax", "")}
                         </div>
                       </Grid>
                       <Grid item>
-                        <div className={classes.center}>IBU</div>
                         <div className={classes.center}>
-                          {_.get(style, "ibuMin")}-{_.get(style, "ibuMax")}
+                          IBU
+                          <Tooltip title="International Bitterness Units">
+                            <HelpOutlineIcon className={classes.helpIcon} />
+                          </Tooltip>
+                        </div>
+                        <div className={classes.center}>
+                          {_.get(style, "ibuMin", "")}-
+                          {_.get(style, "ibuMax", "")}
                         </div>
                       </Grid>
                       <Grid item>
-                        <div className={classes.center}>SRM</div>
                         <div className={classes.center}>
-                          {_.get(style, "srmMin")}-{_.get(style, "srmMax")}
+                          OG
+                          <Tooltip title="Original Gravity">
+                            <HelpOutlineIcon className={classes.helpIcon} />
+                          </Tooltip>
+                        </div>
+                        <div className={classes.center}>
+                          {_.get(style, "ogMin", "")}
                         </div>
                       </Grid>
+                      <Grid item>
+                        <div className={classes.center}>
+                          FG
+                          <Tooltip title="Fermented Gravity">
+                            <HelpOutlineIcon className={classes.helpIcon} />
+                          </Tooltip>
+                        </div>
+                        <div className={classes.center}>
+                          {_.get(style, "fgMin", "")}-
+                          {_.get(style, "fgMax", "")}
+                        </div>
+                      </Grid>
+                      <Grid item>{renderSrm(style)}</Grid>
                       <Grid item>
                         <Typography
                           variant="body2"
@@ -84,5 +116,49 @@ export default function Category() {
     const name = _.get(style, "name", "");
     const shortName = _.get(style, "shortName", "");
     if (shortName && name !== shortName) return `(${shortName})`;
+  }
+
+  function renderSrm(style) {
+    const srmMin = Math.round(_.get(style, "srmMin", ""));
+    const srmMax = Math.round(_.get(style, "srmMax", ""));
+
+    function renderSrmNum() {
+      if (srmMin === srmMax) {
+        return srmMin;
+      } else {
+        return `${srmMin}-${srmMax}`;
+      }
+    }
+
+    return (
+      <div className={classes.srmOuterCont}>
+        <div className={classes.srmInnerCont}>
+          <div className={classes.center}>
+            SRM
+            <Tooltip title="Standard Reference Method">
+              <HelpOutlineIcon className={classes.helpIcon} />
+            </Tooltip>
+          </div>
+          <div className={classes.center}>{renderSrmNum()}</div>
+        </div>
+        <div className={classes.colorBoxCont}>
+          <div
+            style={{
+              backgroundColor: _.get(srmColors, `${srmMin}`, "")
+            }}
+            className={classes.colorBox}
+          />
+          {srmMin === srmMax ? null : (
+            <div
+              style={{
+                backgroundColor: _.get(srmColors, `${srmMax}`, ""),
+                borderTop: "none"
+              }}
+              className={classes.colorBox}
+            />
+          )}
+        </div>
+      </div>
+    );
   }
 }
--- client/src/pages/category/styles.js
+++ client/src/pages/category/styles.js
@@ -15,8 +15,26 @@ export default makeStyles(theme => {
       textAlign: "center",
       fontSize: 14
     },
+    helpIcon: {
+      height: 14,
+      width: 14,
+      marginBottom: "auto",
+      marginLeft: 4
+    },
     description: {
       marginTop: 8
+    },
+    colorBoxCont: {
+      marginLeft: 8
+    },
+    colorBox: {
+      height: 24,
+      width: 24,
+      border: "1px solid black"
+    },
+    srmOuterCont: {
+      display: "flex",
+      alignItems: "center"
     }
   };
 });
